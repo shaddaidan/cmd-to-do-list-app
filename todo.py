9e63acf47cea82bef359eb5953ffc15fbe7ef1c0
Adeniran Shaddai <shaddaidan@gmail.com>
2024-05-18 11:08:06 +0100

    added in the parsers

--- todo.py
+++ todo.py
@@ -1,4 +1,18 @@
+# import required libraries.
+
 import argparse
 import os
 
-# just want to make some changes to remember how this is done yeah thank you.
+# set up argumetn parser
+# -a or --add to add tasks
+# -l or --list to list all tasks
+# -r or --remove to remove tasks using index
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Command_line Todo List App")
+    parser.add_argument("-a", "--add"_, metavar="", help="List all tasks")
+    parser.add_argument("-l", "--list", action="store_true", help="List all tasks")
+    parser.add_argument("-r", "--remove", metavar="", help="remove a task by index")
+    parser.add_argument("-f",)
+    return parser
+                        
