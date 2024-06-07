19a18cc3eef5ac2685acc01db4c270307f7ad8cc
Adeniran Shaddai <shaddaidan@gmail.com>
2024-06-07 18:43:32 +0100

    we can say we have succesfully addded the emphasize command to our file yeah this works nicely now

--- todo.py
+++ todo.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import schedule
 import time
+import threading
 
 # set up argumetn parser
 # -a or --add to add tasks
@@ -78,32 +79,38 @@ def find_word(word):
 
 def emphasize_task(index):
     if os.path.exists("tasks.txt"):
-        with open("tasks.txt", r) as file:
+        with open("tasks.txt", "r") as file:
             tasks = file.readlines()
 
         if 0 < index <= len(tasks):
             task_to_emphasize = tasks.pop(index - 1).strip().upper()
-            tasks.insert(0, task_to_emphasize + '\n')
+            tasks.insert(0, task_to_emphasize + '\n') 
             with open("tasks.txt", "w") as file:
                 file.writelines(tasks)
             print(f"Emphasized task: {task_to_emphasize}")
+            
         else:
             print("invalid index,")
     else:
         print("no tasks found.")
 
-def clear_tasks():
-    if os.path.exists("tasks.txt"):
-        open("tasks.txt", "w").close()
-        print("All tasks have been cleared.")
+# def clear_tasks():
+#     if os.path.exists("tasks.txt"):
+#         open("tasks.txt", "w").close()
+#         print("All tasks have been cleared.")
 
-def schedule_clearing():
-    schedule.every().day.at("03:00").do(clear_tasks)
+# def schedule_clearing():
+#     schedule.every().day.at("03:00").do(clear_tasks)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+#     while True:
+#         schedule.run_pending()
+#         time.sleep(1)
 # parse command line arguments.
+
+# def start_schedule_thread():
+#     clear_thread = threading.Thread(target=schedule_clearing)
+#     clear_thread.daemin = True
+#     clear_thread.start()
 
 def main():
     parser = create_parser()
@@ -123,14 +130,8 @@ def main():
         parser.print_help()
 
     # start the schedule clearing in a seperate thread or process
-    import threading
-    clear_thread = threading.Thread(target=schedule_clearing)
-    clear_thread.daemin = True
-    clear_thread.start()
-
-    # keep the main thread alive
-    while True:
-        time.sleep(1)
+    # if args.add or args.remove or args.emphasize:
+    #     start_schedule_thread()
 
 
 if __name__ == "__main__":
